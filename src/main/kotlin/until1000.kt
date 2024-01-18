1fc9feef585cdb36eae811708587f234de9981e3
PATYA <patysik@mail.ru>
2024-01-18 19:18:53 +0300

    step 10 иная форма

--- src/main/kotlin/until1000.kt
+++ src/main/kotlin/until1000.kt
@@ -1,16 +1,6 @@
-fun main(arrgs:Array<String>) {
-
-    var sum = 0
-    var index = 0
-
+fun main(args:Array<String>) {
     val thousand = arrayOfNulls<Int>(101)
-    do {
-        thousand[index] = sum
-            index++
-        sum += 10
-    } while (index <= 100)
 
-    for (sum in thousand.indices) {
-        println(thousand[sum])
-    }
+    for(thousand in 0..100 step 10)
+        println(thousand)
 }
